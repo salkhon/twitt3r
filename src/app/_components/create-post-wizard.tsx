6650c1355ecbd2a4ae21e54eaf94aa26c52e41c2
salkhon <salmankhondker1998@gmail.com>
2023-11-01 01:48:35 +0600

    post loading state, react toast

--- src/app/_components/create-post-wizard.tsx
+++ src/app/_components/create-post-wizard.tsx
@@ -3,29 +3,31 @@ import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import { LoadingSpinner } from "./loading";
 import { api } from "~/trpc/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import toast from "react-hot-toast";
 
 export function CreatePostWizard() {
   const { isSignedIn, user, isLoaded: userLoaded } = useUser();
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null); // not necessary, keeping to show recommended way
+  const [inputVal, setInputVal] = useState(""); // need state for POST button disabling
 
   const ctx = api.useUtils(); // queryClient methods
 
   const { mutate, isLoading: isPosting } = api.post.create.useMutation({
     onSuccess: () => {
       inputRef.current!.value = "";
+      setInputVal("");
       void ctx.post.getAll.invalidate(); // this is async, void tells TS to ignore the promise's result w/o waiting for it
+    },
+    onError: (err) => {
+      const errMsg = err.data?.zodError?.fieldErrors.content;
+      if (errMsg?.[0]) toast.error(errMsg[0]);
+      else toast.error("Failed to create post: " + err.message);
     },
   });
 
   if (!userLoaded) return <LoadingSpinner size={48} />;
   if (!isSignedIn || !user) return null;
-
-  const handleButtonClick = () => {
-    const text = inputRef.current?.value;
-    if (!text) return;
-    mutate({ content: text });
-  };
 
   return (
     <div className="flex w-full gap-3">
@@ -41,9 +43,29 @@ export function CreatePostWizard() {
         type="text"
         className="grow bg-transparent outline-none"
         ref={inputRef}
+        value={inputVal}
         disabled={isPosting}
+        onChange={(e) => setInputVal(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && mutate({ content: inputVal })}
       />
-      <button onClick={handleButtonClick}>Post</button>
+      {!!inputVal && !isPosting && (
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            const text = inputRef.current?.value;
+            if (!text) return;
+            mutate({ content: text });
+          }}
+        >
+          Post
+        </button>
+      )}
+
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={24} />
+        </div>
+      )}
     </div>
   );
 }
